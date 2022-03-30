1c9cade2b79d20e957f1a0c90e0ae1153c03b8db
Maximilian Faupel <maximilian.faupel@inverso.de>
2022-03-30 14:20:06 +0200

    set high throughput producer configs

--- src/main/java/com/example/kafka/wikimedia/WikimediaChangesProducer.java
+++ src/main/java/com/example/kafka/wikimedia/WikimediaChangesProducer.java
@@ -22,6 +22,12 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, serializer);
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, serializer);
+
+        // set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        
         properties.keySet().stream().forEach((k) -> {
             log.info("{}: {}", k, properties.get(k));
         });
